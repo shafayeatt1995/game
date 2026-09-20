29a3db1bba3bdad67aa85da04045de2e940940d7
shafayeatt1995 <shafayetalanik@gmail.com>
2026-09-20 16:30:34 +0600

    fix: update Cross-Origin-Embedder-Policy header to credentialless

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            value: "credentialless",
           },
         ],
       },
